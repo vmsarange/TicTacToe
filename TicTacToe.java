77a99828fd758364b3dcfe9aa71c52dc2e9316b6
Vaishnavi Sarange <vmsarange@gmail.com>
2024-03-18 14:28:12 +0530

    Filling O and X method

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,8 @@
+import java.util.*;
 class TicTacToe{
+   
+    static HashSet<Integer> userSet = new HashSet<Integer>();
+    static HashSet<Integer> cpuSet = new HashSet<Integer>();
 
 	public static void main(String[] args) {
 		
@@ -11,8 +15,8 @@ class TicTacToe{
 			{' ','|',' ','|',' '}
 			
 		};
+      
 
-		print_Board(g_Board);
 	}
 	static void print_Board(char [][] g_Board){
 
@@ -24,5 +28,76 @@ class TicTacToe{
 			}
 			System.out.println();
 		}
+	}
+
+	static void place_Holder(char [][] g_Board,int position,String user){
+
+		char symbol = ' ';
+
+		if (user.equals("You") {
+			
+			symbol = 'X';
+
+			userSet.add(position);
+		}
+		else if(user.equals("Cpu")){
+
+			symbol = 'O';
+
+			cpuSet.add(position);
+		}
+
+		switch(position){
+
+		case 1 : {
+
+              g_Board[0][0] = symbol;
+              break;
+		  }
+		 case 2 : {
+
+              g_Board[0][2] = symbol;
+              break;
+		  }
+		  case 3 : {
+
+              g_Board[0][4] = symbol;
+              break;
+		  }
+		  case 4 : {
+
+              g_Board[2][0] = symbol;
+              break;
+		  }
+		  case 5 : {
+
+              g_Board[2][2] = symbol;
+              break;
+		  }
+		  case 6 : {
+
+              g_Board[2][4] = symbol;
+              break;
+		  }
+		  case 7 : {
+
+              g_Board[4][0] = symbol;
+              break;
+		  } 
+		  case 8 : {
+
+              g_Board[4][2] = symbol;
+              break;
+		  }
+		case 9 : {
+
+              g_Board[4][4] = symbol;
+              break;
+		  }
+		default : 
+
+			System.out.println("Invalid input!");
+		}
+		print_Board();
 	}
 }
